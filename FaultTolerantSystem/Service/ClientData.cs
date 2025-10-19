f857f19b2c6bd9eb65922dbb9a2074503316a608
Mijat Krivokapić <mijat2004@gmail.com>
2025-10-19 16:23:13 +0200

    Add project setup with service and client

---
+++ FaultTolerantSystem/Service/ClientData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Service
+{
+    public class ClientData
+    {
+        Guid id { get; set; }
+        ClientStatus status { get; set; }
+        DateTime lastHeartbeat { get; set; }
+        ICallback callback { get; set; }
+
+    }
+}
---
+++ FaultTolerantSystem/Service/IService1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Service
+{
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        Guid RegisterClient();
+
+        [OperationContract]
+        void SendHeartbeat(Guid clientId);
+    }
+
+    public interface ICallback
+    {
+       // [OperationContract(IsOneWay = true)]
+        
+    }
+
+
+
+}
