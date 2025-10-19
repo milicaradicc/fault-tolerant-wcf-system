bd52c6690f46de1d0b94ae7e91d095238e132d64
Milica Radic <milica.t.radic@gmail.com>
2025-10-19 11:51:41 +0200

    Create project

--- WcfService/Service/Properties/AssemblyInfo.cs
+++ WcfService/Service/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FaultTolerantServer")]
+[assembly: AssemblyTitle("Service")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FaultTolerantServer")]
+[assembly: AssemblyProduct("Service")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2b34ec99-ecc8-476d-ba75-88345f322bbd")]
+[assembly: Guid("45d4771a-2275-4420-9497-1f27709e2421")]
 
 // Version information for an assembly consists of the following four values:
 //
