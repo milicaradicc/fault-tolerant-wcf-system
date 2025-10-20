2fb2828843368a9caf4c0938e94e9a6a17832057
FilipT03 <filipsombor@gmail.com>
2025-10-20 19:18:44 +0200

    Commit ignored log classes

---
+++ FaultTolerantSystem/Service/Logs/Log.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Service.Clients;
+
+namespace Service.Logs
+{
+    public class Log
+    {
+        public Log() {}
+
+        public Log(Guid clientId, LogType type, string message)
+        {
+            ClientId = clientId;
+            Type = type;
+            Timestamp = DateTime.Now;
+            Message = message;
+
+        }
+
+        public Log(int id, Guid clientId, LogType type, DateTime timestamp, string message)
+        {
+            Id = id;
+            ClientId = clientId;
+            Type = type;
+            Timestamp = timestamp;
+            Message = message;
+        }
+
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [ForeignKey("ClientData")]
+        public Guid? ClientId { get; set; }
+        public virtual ClientData ClientData { get; set; }
+
+        [Required]
+        public LogType Type { get; set; }
+        [Required]
+        public DateTime Timestamp { get; set; }
+        [StringLength(500)]
+        public string Message { get; set; }
+    }
+}
